9ce80bd8896ad2ffe616090c72faac96f37e9852
Emmanuel Pale Molina <epalemolina@gmail.com>
2024-10-19 18:03:32 -0600

    Methods and Tests of UserDb were added

---
+++ PapayagramsServer/Tests/DataAccess/UserDBTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainClasses;
+using LanguageExt;
+using System;
+using System.Data.SqlClient;
+using System.Data.Entity.Core;
+
+namespace DataAccess.Tests
+{
+    [TestClass()]
+    public class UserDBTests
+    {
+        private readonly Player _registeredPlayer = new Player()
+        {
+            Id = 1,
+            Username = "Pale04",
+            Email = "epalemolina@hotmail.com",
+            Password = "040704"
+        };
+
+        [TestInitialize()]
+        public void SetUp()
+        {
+            UserDB.RegisterUser(_registeredPlayer);
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+            using (var context = new papayagramsEntities())
+            {
+                context.Database.ExecuteSqlCommand("delete from [UserStatus]");
+                context.Database.ExecuteSqlCommand("delete from [User]");
+                context.Database.ExecuteSqlCommand("DBCC CHECKIDENT ('User', RESEED, 0)");
+            }
+        }
+
+        [TestMethod()]
+        public void RegisterUserSuccesfulTest()
+        {
+            Player newPlayer = new Player()
+            {
+                Username = "Pale47",
+                Email = "epalemolina@gmail.com",
+                Password = "asdfl_´.468*-"
+            };
+
+            int expected = 2;
+            int result = UserDB.RegisterUser(newPlayer);
+
+            Assert.AreEqual(expected, result, "RegisterUserSuccesfulTest");
+        }
+
+        [TestMethod()]
+        public void RegisterUserEmptyTest()
+        {
+            try
+            {
+                UserDB.RegisterUser(new Player());
+                Assert.Fail("RegisterUserEmptyTest");
+            }
+            catch (Exception error)
+            {
+                Assert.IsInstanceOfType(error, typeof(EntityCommandExecutionException), "RegisterUserEmptyTest");
+            }
+        }
+
+        [TestMethod()]
+        public void LogInSuccessfulTest()
+        {
+            int expected = 0;
+            int result = UserDB.LogIn(_registeredPlayer.Username, _registeredPlayer.Password);
+            Assert.AreEqual(expected, result, "LogInSuccessfulTest");
+        }
+
+        [TestMethod()]
+        public void LogInPasswordIncorrectTest()
+        {
+            int expected = 1;
+            int result = UserDB.LogIn(_registeredPlayer.Username, "123");
+            Assert.AreEqual(expected, result, "LogInPasswordIncorrectTest");
+        }
+
+        [TestMethod()]
+        public void LogInNonExistentAccountTest()
+        {
+            int expected = -1;
+            int result = UserDB.LogIn("Pale", "1");
+            Assert.AreEqual(expected, result, "LogInNonExistentAccountTest");
+        }
+
+        [TestMethod()]
+        public void GetPlayerByUsernameSuccessfulTest()
+        {
+            Option<Player> result = UserDB.GetPlayerByUsername(_registeredPlayer.Username);
+            Assert.AreEqual(_registeredPlayer, result.Case, "GetPlayerByUsernameSuccessfulTest");
+        }
+
+        [TestMethod()]
+        public void GetPlayerByNonExistentTest()
+        {
+            Option<Player> result = UserDB.GetPlayerByUsername("Pale");
+            Assert.IsTrue(result.IsNone, "GetPlayerByNonExistentTest");
+        }
+
+        [TestMethod()]
+        public void GetPlayerByEmailSuccessfulTest()
+        {
+            Option<Player> result = UserDB.GetPlayerByEmail(_registeredPlayer.Email);
+            Assert.AreEqual(_registeredPlayer, result.Case, "GetPlayerByEmailSuccessfulTest");
+        }
+
+        [TestMethod()]
+        public void GetPlayerByEmailInexistentTest()
+        {
+            Option<Player> result = UserDB.GetPlayerByEmail("pale@gmail.com");
+            Assert.IsTrue(result.IsNone, "GetPlayerByEmailInexistentTest");
+        }
+
+        [TestMethod()]
+        public void UpdateUserStatusSuccessfulTest()
+        {
+            int expected = 1;
+            int result = UserDB.UpdateUserStatus(_registeredPlayer.Username, PlayerStatus.online);
+            Assert.AreEqual(expected, result, "UpdateUserStatusSuccessfulTest");
+        }
+
+        [TestMethod()]
+        public void UpdateUserStatusIncorrectUserTest()
+        {
+            int expected = 0;
+            int result = UserDB.UpdateUserStatus("Pale", PlayerStatus.offline);
+            Assert.AreEqual(expected, result, "UpdateUserStatusIncorrectUserTest");
+        }
+    }
+}
--- Papayagrams_Procedures.sql
+++ Papayagrams_Procedures.sql
@@ -1,14 +1,13 @@
-
 CREATE PROCEDURE register_user
     @username VARCHAR(50),
     @email VARCHAR(50),
 	@password VARCHAR(100)
 AS
 BEGIN
-    INSERT INTO [User]
-	(username, email, password)
-	VALUES 
-	(@username, @email, ENCRYPTBYASYMKEY(ASYMKEY_ID('asy_TRIGGERDB'), @password));
+    INSERT INTO [User] (username, email, password) VALUES (@username, @email, ENCRYPTBYASYMKEY(ASYMKEY_ID('asy_TRIGGERDB'), @password));
+	DECLARE @id int
+	SELECT @id = SCOPE_IDENTITY();
+	INSERT INTO [UserStatus] (userId) VALUES (@id);
 END;
 GO
 
@@ -17,13 +16,20 @@ CREATE PROCEDURE log_in
 	@password VARCHAR(100)
 AS
 BEGIN
-	DECLARE @correctPassword VARCHAR(100)
-	SELECT @correctPassword = CAST(DecryptByAsymKey (AsymKey_ID('asy_triggerdb'),password,N'RD_afAmGsRMYi29') AS VARCHAR(100)) 
-	FROM [User] 
-	WHERE username = @username;
-	IF @password = @correctPassword
-	BEGIN 
-		RETURN 0
+	IF EXISTS (SELECT * FROM [User] WHERE username = @username)
+	BEGIN
+		DECLARE @correctPassword VARCHAR(100)
+		SELECT @correctPassword = CAST(DecryptByAsymKey (AsymKey_ID('asy_triggerdb'),password,N'RD_afAmGsRMYi29') AS VARCHAR(100)) 
+		FROM [User] 
+		WHERE username = @username;
+		IF @password = @correctPassword
+		BEGIN 
+			RETURN 0
+		END
+		ELSE
+		BEGIN
+			RETURN 1
+		END
 	END
 	ELSE
 	BEGIN
@@ -50,5 +56,17 @@ BEGIN
 	SELECT id, username, email, CAST(DecryptByAsymKey (AsymKey_ID('asy_triggerdb'),password,N'RD_afAmGsRMYi29') AS VARCHAR(100)) password 
 	FROM [User] 
 	WHERE email = @email;
+END;
+GO
+
+CREATE PROCEDURE update_user_status
+	@username VARCHAR(50),
+	@status VARCHAR(20),
+	@date DATETIME
+AS
+BEGIN
+	DECLARE @userId int
+	SELECT @userId = id FROM [User] WHERE username = @username;
+	UPDATE [UserStatus] SET status = @status, since = @date WHERE userId = @userId;
 END;
 GO
